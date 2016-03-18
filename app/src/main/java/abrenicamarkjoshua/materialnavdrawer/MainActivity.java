e7695e641607fcbde594acfffe6f06d0e2f58e04
joshua <abrenicamarkjoshua@gmail.com>
2016-03-18 17:55:30 +0800

    first commit

---
+++ app/src/main/java/abrenicamarkjoshua/materialnavdrawer/MainActivity.java
@@ -0,0 +1,41 @@
+package abrenicamarkjoshua.materialnavdrawer;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment)
+                getSupportFragmentManager().findFragmentById(R.id.fragment_navigaton_drawer);
+        drawerFragment.setUp((DrawerLayout)findViewById(R.id.drawer_layout), toolbar);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.navigate){
+            startActivity(new Intent(this, SubActivity.class));
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+}
